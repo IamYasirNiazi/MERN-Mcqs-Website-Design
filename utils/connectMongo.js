8b0440d4690efbf9d6ec8f134163d32e51f0f8d8
Yasir Jehangir <iamyasirniazi@gmail.com>
2023-04-07 08:29:06 +0500

    Updated ConnectMongo file for deployment

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,10 +1,26 @@
+// import mongoose from 'mongoose';
+
+// const connectMongo = async () =>{
+//     await mongoose.connect(process.env.MONGODB_URI);
+// }
+
+// export default connectMongo;
+
+
+
+
 import mongoose from 'mongoose';
-
-
 
 const connectMongo = async () =>{
 
-    await mongoose.connect(process.env.MONGODB_URI);
+await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.log('Error connecting to MongoDB', error);
+  });
 
 }
 
